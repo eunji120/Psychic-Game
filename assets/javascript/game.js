5f56fac359ea67b47dbcba0eef9c327ec14be6ab
Eunji Lee <eunjilee@Eunjis-MacBook-Air.local>
2018-06-04 17:14:29 -0400

    Edited files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,10 @@
 //pseudo code
+
+//Variables: Wins, Losses, Guesses Left, Guesses So Far, Computer Guesses
+
+//Generate Computer's random Answer
+
+//Generate the User's guess
+
+//Compare User Guess vs. Computer Answer; if the User guesses right, Win # goes up. If incorrect, then different guesses until guesses left are available. When the user does not get the correct answer after the provided number of guesses, then the Loss # goes up.
+
