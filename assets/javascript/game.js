61d5c8259db19ad8a5f57fdac8732f469d8539b9
Eunji Lee <eunjilee@Eunjis-MacBook-Air.local>
2018-06-04 17:29:26 -0400

    Edited a file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@
 
 //Generate Computer's random Answer
 
+alphabet.Math.floor(Math.random()* alphabet.length) 
+
 //Generate the User's guess
 
 //Compare User Guess vs. Computer Answer; if the User guesses right, Win # goes up. If incorrect, then different guesses until guesses left are available. When the user does not get the correct answer after the provided number of guesses, then the Loss # goes up.
